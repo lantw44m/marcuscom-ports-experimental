a6a95019d9573fecea2c1c1e99e90f43e2bc7c08
adamw <adamw@df743ca5-7f9a-e211-a948-0013205c9059>
2004-08-25 17:25:56 +0000

    Update to 2.5.2.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports-experimental/trunk@17289 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-toolkits/gtk20/files/patch-gtk_gtkentry.c
+++ x11-toolkits/gtk20/files/patch-gtk_gtkentry.c
@@ -1,14 +1,13 @@
---- gtk/gtkentry.c.orig	Sun Aug  8 17:21:07 2004
-+++ gtk/gtkentry.c	Sun Aug  8 17:21:49 2004
-@@ -1592,9 +1592,10 @@
+--- gtk/gtkentry.c.orig	Wed Aug 25 12:44:23 2004
++++ gtk/gtkentry.c	Wed Aug 25 12:44:52 2004
+@@ -1670,8 +1670,9 @@
  				    event->x + entry->scroll_offset, event->y))
  	{
  	  GdkDragContext *context;
 +	  guint actions;
  	  GtkTargetList *target_list = gtk_target_list_new (NULL, 0);
- 	  gtk_target_list_add_text_targets (target_list);
 -	  guint actions = entry->editable ? GDK_ACTION_COPY | GDK_ACTION_MOVE : GDK_ACTION_COPY;
 +	  actions = entry->editable ? GDK_ACTION_COPY | GDK_ACTION_MOVE : GDK_ACTION_COPY;
- 	  
- 	  context = gtk_drag_begin (widget, target_list, actions,
- 			  entry->button, (GdkEvent *)event);
+ 
+ 	  gtk_target_list_add_text_targets (target_list);
+ 
--- x11-toolkits/gtk20/files/patch-gtk_gtkicontheme.c
+++ x11-toolkits/gtk20/files/patch-gtk_gtkicontheme.c
@@ -1,26 +1,11 @@
---- gtk/gtkicontheme.c.orig	Thu Jan 29 16:53:42 2004
-+++ gtk/gtkicontheme.c	Thu Jan 29 16:54:56 2004
-@@ -526,7 +526,7 @@
- 
-   priv->custom_theme = FALSE;
-   priv->current_theme = g_strdup (DEFAULT_THEME_NAME);
--  priv->search_path = g_new (char *, 5);
-+  priv->search_path = g_new (char *, 7);
+--- gtk/gtkicontheme.c.orig	Wed Aug 18 11:45:13 2004
++++ gtk/gtkicontheme.c	Wed Aug 25 12:50:40 2004
+@@ -544,6 +544,8 @@
+   i = 0;
+   priv->search_path[i++] = g_build_filename (g_get_home_dir (), ".icons", NULL);
+   priv->search_path[i++] = g_build_filename (g_get_user_data_dir (), "icons", NULL);
++  priv->search_path[i++] = g_build_filename (g_get_user_data_dir (), "gnome", "pixmaps", NULL);
++  priv->search_path[i++] = g_build_filename (g_get_user_data_dir (), "gnome", "icons", NULL);
    
- 
-   priv->search_path[0] = g_build_filename (g_get_home_dir (),
-@@ -534,9 +534,11 @@
- 					   NULL);
-   priv->search_path[1] = g_build_filename (GTK_DATADIR, "pixmaps", NULL);
-   priv->search_path[2] = g_build_filename (GTK_DATADIR, "icons", NULL);
--  priv->search_path[3] = g_strdup ("/usr/share/icons");
--  priv->search_path[4] = g_strdup ("/usr/share/pixmaps");
--  priv->search_path_len = 5;
-+  priv->search_path[3] = g_build_filename (GTK_DATADIR, "gnome", "pixmaps", NULL);
-+  priv->search_path[4] = g_build_filename (GTK_DATADIR, "gnome", "icons", NULL);
-+  priv->search_path[5] = g_strdup ("/usr/share/icons");
-+  priv->search_path[6] = g_strdup ("/usr/share/pixmaps");
-+  priv->search_path_len = 7;
- 
-   priv->themes_valid = FALSE;
-   priv->themes = NULL;
+   for (j = 0; xdg_data_dirs[j]; j++) 
+     priv->search_path[i++] = g_build_filename (xdg_data_dirs[j], "icons", NULL);
