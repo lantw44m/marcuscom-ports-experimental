ab9ed0da289e0f6ed2286c0ff66b3d6e5f6fc0bc
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2004-09-04 20:26:48 +0000

    Fix icon theme loading.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports-experimental/trunk@17295 df743ca5-7f9a-e211-a948-0013205c9059

--- x11-toolkits/gtk20/files/patch-gtk_gtkicontheme.c
+++ x11-toolkits/gtk20/files/patch-gtk_gtkicontheme.c
@@ -1,11 +1,23 @@
 --- gtk/gtkicontheme.c.orig	Wed Aug 18 11:45:13 2004
-+++ gtk/gtkicontheme.c	Wed Aug 25 12:50:40 2004
-@@ -544,6 +544,8 @@
-   i = 0;
++++ gtk/gtkicontheme.c	Sat Sep  4 16:26:21 2004
+@@ -537,7 +537,7 @@
+   xdg_data_dirs = g_get_system_data_dirs ();
+   for (i = 0; xdg_data_dirs[i]; i++) ;
+ 
+-  priv->search_path_len = i + 3;
++  priv->search_path_len = (i * 2) + 3;
+   
+   priv->search_path = g_new (char *, priv->search_path_len);
+   
+@@ -545,8 +545,10 @@
    priv->search_path[i++] = g_build_filename (g_get_home_dir (), ".icons", NULL);
    priv->search_path[i++] = g_build_filename (g_get_user_data_dir (), "icons", NULL);
-+  priv->search_path[i++] = g_build_filename (g_get_user_data_dir (), "gnome", "pixmaps", NULL);
-+  priv->search_path[i++] = g_build_filename (g_get_user_data_dir (), "gnome", "icons", NULL);
    
-   for (j = 0; xdg_data_dirs[j]; j++) 
+-  for (j = 0; xdg_data_dirs[j]; j++) 
++  for (j = 0; xdg_data_dirs[j]; j++) {
      priv->search_path[i++] = g_build_filename (xdg_data_dirs[j], "icons", NULL);
++    priv->search_path[i++] = g_build_filename (xdg_data_dirs[j], "pixmaps", NULL);
++  }
+ 
+   priv->search_path[i++] = g_strdup ("/usr/share/pixmaps");
+ 
