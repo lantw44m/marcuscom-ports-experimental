a576c6cd124067ff4440937f4d19df4f3ba5b7ca
ahze <ahze@df743ca5-7f9a-e211-a948-0013205c9059>
2006-06-21 02:06:56 +0000

    - Readd mozilla with the idea of using dialog(1) --radiolist with   bsd.radio.mk. The idea is to implement dialog(1) --radiolist in   bsd.raido.mk then merge the code into bsd.options.mk when we get   it.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports-experimental/trunk@17344 df743ca5-7f9a-e211-a948-0013205c9059

---
+++ www/mozilla/bsd.radio.mk
@@ -0,0 +1,23 @@
+#-*- mode: Fundamental; tab-width: 4; -*-
+# ex:ts=4
+#
+# bsd.radio.mk -- Support for dialog(1) --radiolist
+#
+# Created by:	Michael Johnson <ahze@FreeBSD.org>
+#		Shaun Amott <shaun@FreeBSD.org>
+# Date:         20 June 2006
+#
+# Example:
+#
+# RADIO_OPTIONS= gecko dbm
+#
+# gecko_RADIO_OPTIONS=	firefox "Firefox support" \
+#			mozilla "mozilla support"
+#
+#
+# dbm_RADIO_OPTIONS=	db2 "Berkeley db2 support" \
+#			db3 "Berkeley db3 support"
+# .include <bsd.port.mk>
+
+# $FreeBSD$
+#    $MCom$
