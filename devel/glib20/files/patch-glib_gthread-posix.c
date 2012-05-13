27429b803ffd913ba8cc80ee59eb9d494697d669
marcus <marcus@df743ca5-7f9a-e211-a948-0013205c9059>
2012-05-13 22:34:47 +0000

    Make this patch a bit more portable.
    git-svn-id: svn://creme-brulee.marcuscom.com/ports-experimental/trunk@17440 df743ca5-7f9a-e211-a948-0013205c9059

--- devel/glib20/files/patch-glib_gthread-posix.c
+++ devel/glib20/files/patch-glib_gthread-posix.c
@@ -1,23 +1,20 @@
---- glib/gthread-posix.c.orig	2012-05-07 20:32:49.000000000 -0500
-+++ glib/gthread-posix.c	2012-05-07 20:33:45.000000000 -0500
-@@ -89,20 +89,9 @@
+--- glib/gthread-posix.c.orig	2012-05-13 18:23:49.000000000 -0400
++++ glib/gthread-posix.c	2012-05-13 18:30:37.000000000 -0400
+@@ -89,7 +89,7 @@ g_mutex_impl_new (void)
    if G_UNLIKELY (mutex == NULL)
      g_thread_abort (errno, "malloc");
  
 -#ifdef PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP
--  pthread_mutexattr_t attr;
--  pthread_mutexattr_init (&attr);
--  pthread_mutexattr_settype (&attr, PTHREAD_MUTEX_ADAPTIVE_NP);
--  pattr = &attr;
--#endif
--
++#if defined(PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP) && !defined(__FreeBSD__)
+   pthread_mutexattr_t attr;
+   pthread_mutexattr_init (&attr);
+   pthread_mutexattr_settype (&attr, PTHREAD_MUTEX_ADAPTIVE_NP);
+@@ -99,7 +99,7 @@ g_mutex_impl_new (void)
    if G_UNLIKELY ((status = pthread_mutex_init (mutex, pattr)) != 0)
      g_thread_abort (status, "pthread_mutex_init");
  
 -#ifdef PTHREAD_ADAPTIVE_MUTEX_NP
--  pthread_mutexattr_destroy (&attr);
--#endif
--
-   return mutex;
- }
++#if defined(PTHREAD_ADAPTIVE_MUTEX_NP) && !defined(__FreeBSD__)
+   pthread_mutexattr_destroy (&attr);
+ #endif
  
